df0e0475e90d9e309aca80e7753a71e4f0c0afea
Lithium87 <cibs@abv.bg>
2020-08-14 21:47:49 +0300

    Build the featured part

--- .history/src/App_20200814195642.js
+++ .history/src/App_20200814195642.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Header from './components/header_footer/Header';
-import Featured from './components/featured';
 
 import './resources/styles.css';
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App" style={{height: '1500px', background: 'cornflowerblue'}}>
       <Header />
-      <Featured />
     </div>
   );
 }
---
+++ .history/src/components/featured/TimeUntil_20200814210438.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class TimeUntil extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div className="countdown_wrapper">
+                <div className="countdown_top">
+                    Event starts in
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TimeUntil;
