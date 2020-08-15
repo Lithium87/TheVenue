eb28b53822602940d0abadcc8f70f9a4d06a6478
Lithium87 <cibs@abv.bg>
2020-08-15 18:40:36 +0300

    Build the venue info component

---
+++ .history/src/components/venueNfo/index_20200815181546.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import icon_calendar from '../../resources/images/icons/calendar.png';
+import icon_location from '../../resources/images/icons/location.png';
+
+const VenueNfo = () => {
+    return (
+        <div className="bck_black">
+            hahaha
+        </div>
+    );
+};
+
+export default VenueNfo;
---
+++ .history/src/components/venueNfo/index_20200815182233.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import icon_calendar from '../../resources/images/icons/calendar.png';
+import icon_location from '../../resources/images/icons/location.png';
+
+const VenueNfo = () => {
+    return (
+        <div className="bck_black">
+            <div className="center_wrapper">
+                <div className="vn_wrapper">
+                    <div className="vn_item">
+                        <div className="vn_outer">
+                            <div className="vn_inner">
+                                <div className="vn_icon_square bck_red"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default VenueNfo;
